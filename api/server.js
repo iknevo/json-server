4092e6b96036be2d19bfb41627ae80e7445f9299
idknevo <nevoexists@gmail.com>
2025-01-19 09:54:31 +0200

    edit

--- api/server.js
+++ api/server.js
@@ -17,12 +17,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "api",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "api",
+//     "/blog/:resource/:id/show": "/:resource/:id",
+//   })
+// );
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
